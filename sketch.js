083ed5bc0178e9fb01ef366678ee88c08e7f21e1
hforal <77546839+hforal@users.noreply.github.com>
2021-03-15 13:51:28 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup()
+    {
+        createCanvas (700,800);  
+    }
+    function draw()
+    {
+        background(120);
+        fill('rgb(0,255,0)');
+        circle(350,125,200);
+        rect(250,215,200,250);
+        fill('#222222');
+        circle(315,100,10);
+        circle(385,100,10);
+        circle(350,175,30);
+        arc(350,100,10)
+        fill('rgb(0,255,0)');
+        rect(200,250,50,150);
+        rect(450,250,50,150);
+        rect(250,465,50,200);
+        rect(400,465,50,200);
+    
+
+
+    
+    }
